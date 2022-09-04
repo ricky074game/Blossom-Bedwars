baca648b3d86018b5088b211a2a87cf984bf027a
Tuvqlu <98550458+Tuvqlu@users.noreply.github.com>
2022-09-03 22:33:07 -0400

    add ban commands

--- src/BedWars/game/GameListener.php
+++ src/BedWars/game/GameListener.php
@@ -131,6 +131,28 @@ class GameListener implements Listener
     		}
     	}
     }
+	
+    /**
+     * @param PlayerCommandPreprocessEvent $event
+     */
+  public function onCommandPreprocess(PlayerCommandPreprocessEvent $event)
+    {
+        $msg = $event->getMessage();
+        $player = $event->getPlayer();
+	$dataPlayer = $this->getDataPlayer($player);
+
+        $inGame = $dataPlayer->isInGame();
+
+        if (!$inGame) return;
+
+        $cmd = explode(" ", $msg)[0];
+
+        if (in_array($cmd, $this->plugin->getConfig()->get("Banned-Commands"))) {
+            $player->sendMessage($this->plugin->getConfig()->get("Banned-Command-Message"));
+            $event->cancel();
+        }
+    }
+	
 
     public function onExhaust(PlayerExhaustEvent $ev) : void{
         $player = $ev->getPlayer();
